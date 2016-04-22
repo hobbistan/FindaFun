42dd7e1f3625788e2d371eafd6a434d61fc15a6d
nandhakumar30 <datacrawl.nandha@gmail.com>
2016-04-22 18:20:29 +0530

    Added new java file for forgot password

---
+++ app/src/main/java/com/findafun/activity/ForgotPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.findafun.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Data Crawl 6 on 22-04-2016.
+ */
+public class ForgotPasswordActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
