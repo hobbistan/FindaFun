49ce128768b28e583dc0e8af72d7a8ace1ba4f38
DataCrawl <datacrawl.manoj@gmail.com>
2016-04-26 19:10:47 +0530

    update image banner zoom concept:       activity_event_detail.xml       EventDetailActivity.java

--- app/src/main/java/com/findafun/activity/EventDetailActivity.java
+++ app/src/main/java/com/findafun/activity/EventDetailActivity.java
@@ -1,24 +1,20 @@
 package com.findafun.activity;
 
-import com.findafun.AppRate.AppRate;
 import android.Manifest;
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Point;
-import android.graphics.Rect;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -31,9 +27,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.DecelerateInterpolator;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,12 +44,15 @@ import com.facebook.FacebookException;
 import com.facebook.share.Sharer;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
-
+import com.findafun.AppRate.AppRate;
 import com.findafun.R;
 import com.findafun.app.AppController;
 import com.findafun.bean.events.Event;
 import com.findafun.bean.gamification.GamificationDataHolder;
+import com.findafun.fragment.ViewPagerFragment;
 import com.findafun.helper.FindAFunHelper;
+import com.findafun.photowidget.ImageInfo;
+import com.findafun.photowidget.PhotoView;
 import com.findafun.servicehelpers.EventServiceHelper;
 import com.findafun.servicehelpers.ShareServiceHelper;
 import com.findafun.serviceinterfaces.IEventServiceListener;
@@ -64,7 +67,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.imageaware.ImageAware;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import org.json.JSONObject;
 
@@ -82,7 +90,7 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
     private TextView txtEventName, txtEventCategory, txtEventDesc, txtEventVenue, txtEventStartDate, txtEventEndDate, txtEventStartTime, txtEventEndTime;
     private TextView txtEventTime, txtEventDate, txtEventEntry, txtEventContact, txtEventEmail, txtWebSite;
     private TextView txtViewMore,txtViewLess;
-    private ImageView imgEventBanner;
+    private GridView imgEventBanner;
     ImageLoader uImageLoader = AppController.getInstance().getUniversalImageLoader();
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
@@ -97,6 +105,8 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
     private List<String> mShareLIst = new ArrayList<String>();
     private List<Integer> mShareResources = new ArrayList<Integer>();
     private List<Integer> mShareBagroundResources = new ArrayList<Integer>();
+    private ArrayList<String> imgList = new ArrayList<>();
+    private ArrayList<ImageInfo> imgImageInfos = new ArrayList<>();
     int curRate;
     private int mShortAnimationDuration;
     @Override
@@ -104,7 +114,10 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.slide_right, 0);
         setContentView(R.layout.activity_event_detail);
-
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -359,7 +372,8 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
             txtEventEmail.setText("N/A");
         }
         Log.d(TAG, "Image uri is" + event.getEventBanner());
-        uImageLoader.displayImage((event.getEventLogo()), imgEventBanner);
+        uImageLoader.displayImage((event.getEventLogo()), (ImageAware) imgEventBanner);
+        imgList.add(0, event.getEventLogo());
     }
 
     private void showShareList() {
@@ -461,17 +475,46 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
         txtEventEndDate = (TextView) findViewById(R.id.cal_to_val);
         txtEventStartTime = (TextView) findViewById(R.id.txt_clock_from_val);
         txtEventEndTime = (TextView) findViewById(R.id.clock_to_val);
-        imgEventBanner = (ImageView) findViewById(R.id.img_banner);
-        final View bannerClick = findViewById(R.id.img_banner);
-      /*  imgEventBanner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        imgEventBanner = (GridView) findViewById(R.id.img_banner);
 
-                int eventBannerImage = Integer.parseInt(event.getEventBanner());
-            //    zoomImageFromThumb(bannerClick, eventBannerImage);
+
+
+      //  imgList.add(0, event.getEventLogo());
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(EventDetailActivity.this));
+        ImageAdapter adapter = new ImageAdapter();
+        imgEventBanner.setAdapter(adapter);
+
+        imgEventBanner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (view.isEnabled()) {
+                    Bundle bundle = new Bundle();
+                    bundle.putStringArrayList("imgs", imgList);
+                    bundle.putParcelable("info", ((PhotoView) view).getInfo());
+                    bundle.putInt("position", position);
+                    imgImageInfos.clear();
+                    //NOTE:if imgList.size >= the visible count in single screen,i will cause NullPointException
+                    //because item out of screen have been replaced/reused
+                    for (int i = 0; i < imgList.size(); i++) {
+                        imgImageInfos.add(((PhotoView) parent.getChildAt(i)).getInfo());
+                    }
+                    parent.getChildAt(position);
+                    bundle.putParcelableArrayList("infos", imgImageInfos);
+                    getSupportFragmentManager().beginTransaction().replace(Window.ID_ANDROID_CONTENT, ViewPagerFragment.getInstance(bundle), "ViewPagerFragment")
+                            .addToBackStack(null).commit();
+                }
+
             }
         });
-*/
+
+
+
+
+
+
+
+
+
 
 
         callbackManager = CallbackManager.Factory.create();
@@ -530,6 +573,84 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
             }
         });
     }
+
+
+    class ImageAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return imgList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+
+
+
+            PhotoView p = new PhotoView(EventDetailActivity.this);
+
+           p.setLayoutParams(new AbsListView.LayoutParams((int) (getResources().getDisplayMetrics().density * 400), (int) (getResources().getDisplayMetrics().density * 400)));
+           p.setScaleType(ImageView.ScaleType.FIT_XY);
+            p.setEnabled(false);
+try {
+    //get thumbnailurl to save user data...like WeChat does
+    String thumbnailUrl = getThumbnailImageUrl(imgList.get(i), 0, 0);
+
+    uImageLoader.displayImage(thumbnailUrl, p,
+            new DisplayImageOptions.Builder()
+                    .showImageOnLoading(android.R.color.darker_gray)
+                    .cacheInMemory(true).cacheOnDisk(true).build(), loadingListener);
+
+   /* ImageLoader.getInstance().displayImage(thumbnailUrl, p,
+            new DisplayImageOptions.Builder()
+                    .showImageOnLoading(android.R.color.darker_gray)
+                    .cacheInMemory(true).cacheOnDisk(true).build(), loadingListener);*/
+   p.setAdjustViewBounds(true);
+    p.touchEnable(false);//disable touch
+} catch (Exception e){
+    e.printStackTrace();
+}
+            return p;
+        }
+    }
+
+    private ImageLoadingListener loadingListener = new SimpleImageLoadingListener() {
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            view.setEnabled(true);//only loadedImage is available we can click item
+        }
+    };
+
+
+    public String getThumbnailImageUrl(String imgUrl,int width,int height){
+        String url="http://imgsize.ph.126.net/?imgurl=data1_data2xdata3x0x85.jpg&enlarge=true";
+        width = (int) (getResources().getDisplayMetrics().density * 100);
+        height = (int) (getResources().getDisplayMetrics().density * 100); //just for convenient
+        url=url.replaceAll("data1", imgUrl).replaceAll("data2", width+"").replaceAll("data3", height+"");
+        return url;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //just for test to clean cache
+        ImageLoader.getInstance().clearMemoryCache();
+        ImageLoader.getInstance().clearDiskCache();
+    }
+
+
 
     private void sendShareStatustoServer(){
         ShareServiceHelper serviceHelper = new ShareServiceHelper(this);
@@ -664,7 +785,7 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
 
     @Override
     public void onError(String erorr) {
-        Log.d(TAG,"error while sending status update"+ erorr);
+        Log.d(TAG, "error while sending status update" + erorr);
 
     }
 
@@ -677,7 +798,7 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
 
     @Override
     public void onEventError(String error) {
-        Log.e(TAG,"Error while bookmarking event");
+        Log.e(TAG, "Error while bookmarking event");
         Toast.makeText(this,"Error bookmarking event", Toast.LENGTH_SHORT).show();
 
     }
@@ -852,143 +973,7 @@ public class EventDetailActivity extends AppCompatActivity implements GoogleApiC
     }
 
 
-    private void zoomImageFromThumb(final View thumbView, int imageResId) {
-        // If there's an animation in progress, cancel it
-        // immediately and proceed with this one.
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
-        }
 
-        // Load the high-resolution "zoomed-in" image.
-        final ImageView expandedImageView = (ImageView) findViewById(
-                R.id.expanded_image);
-        /*expandedImageView.setImageBitmap(imageResId);*/
-
-        // Calculate the starting and ending bounds for the zoomed-in image.
-        // This step involves lots of math. Yay, math.
-        final Rect startBounds = new Rect();
-        final Rect finalBounds = new Rect();
-        final Point globalOffset = new Point();
-
-        // The start bounds are the global visible rectangle of the thumbnail,
-        // and the final bounds are the global visible rectangle of the container
-        // view. Also set the container view's offset as the origin for the
-        // bounds, since that's the origin for the positioning animation
-        // properties (X, Y).
-        thumbView.getGlobalVisibleRect(startBounds);
-        findViewById(R.id.event_detail_container)
-                .getGlobalVisibleRect(finalBounds, globalOffset);
-        startBounds.offset(-globalOffset.x, -globalOffset.y);
-        finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-        // Adjust the start bounds to be the same aspect ratio as the final
-        // bounds using the "center crop" technique. This prevents undesirable
-        // stretching during the animation. Also calculate the start scaling
-        // factor (the end scaling factor is always 1.0).
-        float startScale;
-        if ((float) finalBounds.width() / finalBounds.height()
-                > (float) startBounds.width() / startBounds.height()) {
-            // Extend start bounds horizontally
-            startScale = (float) startBounds.height() / finalBounds.height();
-            float startWidth = startScale * finalBounds.width();
-            float deltaWidth = (startWidth - startBounds.width()) / 2;
-            startBounds.left -= deltaWidth;
-            startBounds.right += deltaWidth;
-        } else {
-            // Extend start bounds vertically
-            startScale = (float) startBounds.width() / finalBounds.width();
-            float startHeight = startScale * finalBounds.height();
-            float deltaHeight = (startHeight - startBounds.height()) / 2;
-            startBounds.top -= deltaHeight;
-            startBounds.bottom += deltaHeight;
-        }
-
-        // Hide the thumbnail and show the zoomed-in view. When the animation
-        // begins, it will position the zoomed-in view in the place of the
-        // thumbnail.
-        thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
-
-        // Set the pivot point for SCALE_X and SCALE_Y transformations
-        // to the top-left corner of the zoomed-in view (the default
-        // is the center of the view).
-        expandedImageView.setPivotX(0f);
-        expandedImageView.setPivotY(0f);
-
-        // Construct and run the parallel animation of the four translation and
-        // scale properties (X, Y, SCALE_X, and SCALE_Y).
-        AnimatorSet set = new AnimatorSet();
-        set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
-                        startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
-                        startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
-                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
-                View.SCALE_Y, startScale, 1f));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                mCurrentAnimator = null;
-            }
-        });
-        set.start();
-        mCurrentAnimator = set;
-
-        // Upon clicking the zoomed-in image, it should zoom back down
-        // to the original bounds and show the thumbnail instead of
-        // the expanded image.
-        final float startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
-                }
-
-                // Animate the four positioning/sizing properties in parallel,
-                // back to their original values.
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.Y,startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
-            }
-        });
-    }
 
 
 
