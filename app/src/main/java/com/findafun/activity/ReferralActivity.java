05586f385f32437807240ab52897d6fdca75d3e5
nandhakumar30 <datacrawl.nandha@gmail.com>
2016-12-05 10:49:33 +0530

    05 Dec 2016

---
+++ app/src/main/java/com/findafun/activity/ReferralActivity.java
@@ -0,0 +1,18 @@
+package com.findafun.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nandha on 04-12-2016.
+ */
+
+public class ReferralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
